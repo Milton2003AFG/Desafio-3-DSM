f060bea94851eefe48ab2dc4fdf5a515a6aa730a
Milton2003AFG <miltonflores2003@gmail.com>
2026-05-07 14:45:52 -0600

    Corregir: RetrofitClient.kt

--- app/src/main/java/com/example/desafio3dsm/network/RetrofitClient.kt
+++ app/src/main/java/com/example/desafio3dsm/network/RetrofitClient.kt
@@ -1,11 +1,11 @@
 package com.example.desafio3dsm.network
 
-import com.google.firebase.appdistribution.gradle.ApiService
+import com.example.desafio3dsm.network.ApiService
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "https://68ea85d0f1eeb3f856e78358.mockapi.io/api/recursos"
+    private const val BASE_URL = "https://68ea85d0f1eeb3f856e78358.mockapi.io/api/"
 
     val instance: ApiService by lazy {
         Retrofit.Builder()
