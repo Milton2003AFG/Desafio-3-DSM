dfc5c856b16c993a2b77badeeab06a25aa9309bc
Milton2003AFG <miltonflores2003@gmail.com>
2026-05-09 12:52:37 -0600

    Agregar: PasswordHasher.kt y AndroidManifest.xml

---
+++ app/src/main/java/com/example/desafio3dsm/utils/PasswordHasher.kt
@@ -0,0 +1,9 @@
+package com.example.desafio3dsm.utils
+
+import java.security.MessageDigest
+object PasswordHasher {
+    fun hash(password: String): String {
+        val bytes = MessageDigest.getInstance("SHA-256").digest(password.toByteArray())
+        return bytes.joinToString("") { "%02x".format(it) }
+    }
+}
